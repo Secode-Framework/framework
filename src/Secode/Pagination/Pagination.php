5dcefd78f718777e8b27402729ef9826ad1388c2
Hans Burg De La Cruz Acosta <hansdelacruzacosta@gmail.com>
2022-12-31 18:46:59 -0500

    Xd

---
+++ src/Secode/Pagination/Pagination.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Secode\Pagination;
+
+/**
+ *
+ */
+class Pagination
+{
+    /**
+     * @var int|null
+     */
+    private ?int $offset = 0;
+    /**
+     * @var int|null
+     */
+    private ?int $pageSize = 10;
+    /**
+     * @var int|null
+     */
+    private ?int $page = 1;
+    /**
+     * @var array|null
+     */
+    private ?array $sort = null;
+
+    /**
+     * @return int|null
+     */
+    public function getOffset(): ?int
+    {
+        return $this->offset;
+    }
+
+    /**
+     * @param int|null $offset
+     * @return $this
+     */
+    public function setOffset(?int $offset): self
+    {
+        $this->offset = $offset;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getPageSize(): ?int
+    {
+        return $this->pageSize;
+    }
+
+    /**
+     * @param int|null $pageSize
+     * @return $this
+     */
+    public function setPageSize(?int $pageSize): self
+    {
+        $this->pageSize = $pageSize;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getPage(): ?int
+    {
+        return $this->page;
+    }
+
+    /**
+     * @param int|null $page
+     * @return $this
+     */
+    public function setPage(?int $page): self
+    {
+        $this->page = $page;
+        return $this;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getSort(): ?array
+    {
+        return $this->sort;
+    }
+
+    /**
+     * @param array|null $sort
+     * @return $this
+     */
+    public function setSort(?array $sort): self
+    {
+        $this->sort = $sort;
+        return $this;
+    }
+
+
+    public function toResponse($collector)
+    {
+        $cloneCollector = clone $collector;
+
+        $collector = $collector->skip($this->getOffset())
+            ->take($this->getPageSize());
+
+        if ($sort = $this->getSort()) {
+            //Agregar el sort
+            $collector = $collector->orderBy($sort['col'], $sort['dir']);
+        } else {
+            $collector = $collector->orderBy('id', 'desc');
+        }
+
+        $totalRows = $cloneCollector->count();
+        $lastPage = ceil($totalRows / $this->getPageSize());
+
+        return [
+            'lastPage' => $lastPage,
+            'totalRows' => $totalRows,
+            'data' => $collector->get()
+        ];
+    }
+}
---
+++ src/Secode/Pagination/PaginationMapper.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Secode\Pagination;
+
+class PaginationMapper
+{
+    public static function requestToPagination(): Pagination
+    {
+        $sortCol = request('sortCol');
+        $sortDir = request('sortDir');
+
+        if ($sortCol && in_array($sortDir, ['asc', 'desc'])) {
+            $sort = ['col' => self::camelToSnake($sortCol), 'dir' => $sortDir];
+        } else {
+            $sort = null;
+        }
+
+        $pageSize = request('pageSize') ?? 100;
+        $page = request('page') ?? 1;
+
+        if ($page == 1) {
+            $offset = 0;
+        } else {
+            $offset = ($page - 1) * $pageSize;
+        }
+
+        return (new Pagination())
+            ->setOffset($offset)
+            ->setPage($page)
+            ->setPageSize($pageSize)
+            ->setSort($sort);
+    }
+
+    public static function camelToSnake($str)
+    {
+
+        $result = "";
+
+        if (strlen($str)) {
+
+            $c = $str[0];
+            $result = $result . strtolower($c);
+
+            for ($i = 1; $i < strlen($str); $i++) {
+
+                $ch = $str[$i];
+
+                if ($ch === strtoupper($ch)) {
+                    $result .= '_';
+                }
+
+                $result .= strtolower($ch);
+
+            }
+        }
+
+        return $result;
+    }
+}
